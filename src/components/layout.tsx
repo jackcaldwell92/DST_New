1f975bb13b877e0e34d1b397b0414dec74d58011
Jack Caldwell <jackcaldwell@Jacks-MacBook-Pro.local>
2018-11-18 12:53:39 +0000

    First Commit

---
+++ src/components/layout.tsx
@@ -0,0 +1,52 @@
+import { graphql, StaticQuery } from 'gatsby';
+import * as React from 'react';
+import Helmet from 'react-helmet';
+
+import {
+  createGlobalStyle,
+  ThemeProvider,
+} from '../styled-components/styled-components';
+import { theme } from '../styled-components/theme';
+
+const GlobalStyle = createGlobalStyle`
+  margin: 10px;
+`;
+
+interface IProps {
+  children: JSX.Element;
+}
+
+const Layout = ({ children }: IProps) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            { name: 'description', content: 'Sample' },
+            { name: 'keywords', content: 'sample, something' },
+          ]}
+        >
+          <html lang="en" />
+        </Helmet>
+        <ThemeProvider theme={theme}>
+          <>
+            <div>{children}</div>
+            <GlobalStyle />
+          </>
+        </ThemeProvider>
+      </>
+    )}
+  />
+);
+
+export default Layout;
